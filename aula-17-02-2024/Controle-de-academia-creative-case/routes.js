5fff3984ac878535dbf7ee0f30f74c6bc86d6c9d
Fernando Lucas de Castro Cavalcante <117499108+massancode@users.noreply.github.com>
2024-03-18 09:12:48 -0300

    Controle-de-Academia
    Controle de academia

---
+++ aula-17-02-2024/Controle-de-academia-creative-case/routes.js
@@ -0,0 +1,19 @@
+    const express = require ('express')
+    const routes = express.Router()
+
+
+    routes.get ('/',function(req,res){
+        return res.render ("layout")
+    })
+
+    routes.get ('/instructors',function(req,res){
+        return res.render ("instructors/index")
+    })
+
+    routes.get ('/members',function(req,res){
+        return res.render ("members")
+    })
+
+
+
+    module.exports = routes
